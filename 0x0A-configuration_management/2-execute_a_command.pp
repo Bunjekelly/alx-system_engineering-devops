0d1847e19cea2b11a0e1028ccd62a1315a4be1df
Bunje Kelly <kellybunje@gmail.com>
2023-09-22 12:03:13 -0700

    killmenow

---
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -0,0 +1,7 @@
+# creating a manifest that kills a process named killmenow using puppet
+
+exec { 'kill_process':
+  command => 'pkill -f killmenow',
+  path    => ['/bin', '/usr/bin'],
+  onlyif  => 'pgrep -f killmenow',
+}
