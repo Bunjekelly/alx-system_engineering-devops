ae08ed89309a50fa04c85094755ddbf7b796089b
Bunje Kelly <kellybunje@gmail.com>
2023-10-24 11:05:50 -0700

    changes

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -6,21 +6,21 @@ import sys
 
 if __name__ == '__main__':
     user_id = sys.argv[1]
-
-    employee_url = 'http://jsonplaceholder.typicode.com/users/{}'.format(user_id)
-    tasks_url = 'http://jsonplaceholder.typicode.com/todos?userId={}'.format(user_id)
+    
+    api_url = 'http://jsonplaceholder.typicode.com'
+    employee_url = '{}/users/{}'.format(api_url, user_id)
+    tasks_url = '{}/todos?userId={}'.format(api_url, user_id)
     employee = requests.get('{}'.format(employee_url)).json()
     tasks = requests.get('{}'.format(tasks_url)).json()
 
-    tasks_completed = []
+    tasks_completed = 0
     for task in tasks:
         if task.get("completed"):
-            tasks_completed.append(task.get("title"))
-
-    completed_count = len(tasks_completed)
+            tasks_completed += 1
     total_count = len(tasks)
 
     print("Employee {} is done with tasks({}/{}):".format(
-        employee.get("name"), completed_count, total_count))
-    for task in tasks_completed:
-        print("\t" + " " + task)
+        employee.get("name"), tasks_completed, total_count))
+    for task in tasks:
+        if task.get("completed"):
+            print("\t " + task.get("title"))
